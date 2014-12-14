598ab6779d08f0c355606c720825a1ddc2ad6931
Ruben Vermeersch <ruben@rocketeer.be>
2014-12-14 16:39:09 +0100

    Make sure ranges don't overlap.

--- mapper_test.go
+++ mapper_test.go
@@ -42,6 +42,19 @@ func TestLatin(t *testing.T) {
 	}
 }
 
+func TestEdges(t *testing.T) {
+	edges := []rune{'\u00FF', '\u01FF', '\u1FFF', '\uFFFF', '\U0001FFFF', '\U0002FFFF'}
+	var lower int32 = 0
+	var upper int32 = math.MaxInt32
+	for edge := range edges {
+		result, _ := utf8mapper.MapString(string(edge), lower, upper)
+		result2, _ := utf8mapper.MapString(string(edge+1), lower, upper)
+		if result >= result2 {
+			t.Errorf("Overlapping results around edges!")
+		}
+	}
+}
+
 func BenchmarkHello(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		utf8mapper.MapString("hello", 0, math.MaxInt32)
